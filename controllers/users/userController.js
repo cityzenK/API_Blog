945686a73a91db71844c447307f1fa0e7f6c9969
cityzenK <eliezergoh@gmail.com>
2024-12-06 15:47:03 -0600

    Add: Change user login

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -1,4 +1,6 @@
-import User from "../../model/User/User.js"
+import User from "../../model/User/User.js";
+import bcrypt from "bcrypt";
+import { createToken } from "../../utils/generateToken.js";
 
 export const userRegisterCtrl = async (req, res) => {
   const {
@@ -16,13 +18,15 @@ export const userRegisterCtrl = async (req, res) => {
       })
     }
     //TODO: Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     //create the user
     const user = await User.create({
       firstname,
       lastname,
       email,
-      password
+      password: hashedPassword
     });
 
     res.json({
@@ -44,18 +48,21 @@ export const logginUser = async (req, res) => {
       });
     }
 
-    const isPasswordMatched = await User.findOne({ password });
-    if (!isPasswordMatched) {
+    const isPasswordMatched = await bcrypt.compare(password, userFound.password);
+    if (!userFound || !isPasswordMatched) {
       return res.json({
         msg: "Wrong login credentials"
       });
     }
 
-
-
     res.json({
       status: "success",
-      data: "user login",
+      data: {
+        firstname: userFound.firstname,
+        lastname: userFound.lastname,
+        email: userFound.email,
+        token: createToken(userFound._id)
+      }
     });
   } catch (error) {
     res.json(error.message);
@@ -74,11 +81,12 @@ export const userController = async (req, res) => {
 }
 
 export const userProfile = async (req, res) => {
+  const { id } = req.params
   try {
-    const user = req.params;
+    const user = await User.findById(id);
     res.json({
       status: "success",
-      data: `profile of user ${user.id}`,
+      data: user
     })
   } catch (error) {
     res.json(error.message)
